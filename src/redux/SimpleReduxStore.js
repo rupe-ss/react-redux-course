8f101660bb272a60097b1fd98596c0c51038558d
rupesss-09 <rupesss09@gmail.com>
2021-12-04 13:49:56 -0600

    Creating reducer and dispatching the reducer

--- src/redux/SimpleReduxStore.js
+++ src/redux/SimpleReduxStore.js
@@ -1,15 +1,3 @@
-//Defining Action, its just a function
 
-//Action Increment
-const increment = () => {
-    return {
-        type: 'INCREMENT', // Name of action
-    };
-};
 
-//Action Decrement
-const decrement = () => {
-    return {
-        type: 'DECREMENT', //Name of another action
-    };
-};
+
