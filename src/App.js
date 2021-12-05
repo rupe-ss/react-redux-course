039880b0121329a32d2d3b60f251e7be2a8cd028
rupesss-09 <rupesss09@gmail.com>
2021-12-04 21:41:04 -0600

    used useSelector from react-redux to display state in UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+    const counter = useSelector((state) => state.counter);
+    const isLogged = useSelector((state) => state.isLogged);
+
     return (
-        <div
-            className='App'
-            style={{ color: 'red', fontWeight: 'bold', fontSize: 40 }}>
-            Header
+        <div className='App'>
+            <div style={{ color: 'red', fontWeight: 'bold', fontSize: 40 }}>
+                {counter}
+            </div>
+            <button style={{ padding: '10px' }}>+</button>
+            <button style={{ padding: '10px' }}>-</button>
+            {isLogged ? <div> Logged In </div> : ''}
         </div>
     );
 }
