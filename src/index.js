dc0ad991135f25f32d3070745e272cf60a8fe5ca
rupesss-09 <rupesss09@gmail.com>
2021-12-04 20:39:48 -0600

    Combined allReducers and imported in main index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import allReducers from './reducers';
 
 import { createStore } from 'redux';
 
-const globleStore = createStore();
+const globleStore = createStore(allReducers);
 
 ReactDOM.render(<App />, document.getElementById('root'));
