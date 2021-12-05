3590e7abf93cf43b23835b259f5fa71db2a79ca2
rupesss-09 <rupesss09@gmail.com>
2021-12-04 20:45:29 -0600

    Added redux devtool extension

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import allReducers from './reducers';
 
 import { createStore } from 'redux';
 
-const globleStore = createStore(allReducers);
+const globleStore = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
