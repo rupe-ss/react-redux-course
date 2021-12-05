aca02b9eb8e5d4d8c69f1ef464f2f7c8244686ec
rupesss-09 <rupesss09@gmail.com>
2021-12-04 20:28:13 -0600

    Actions are defined in index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import { createStore } from 'redux';
+
+const globleStore = createStore();
+
 ReactDOM.render(<App />, document.getElementById('root'));
